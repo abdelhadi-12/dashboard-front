c223ce210b97330e13c43a424831785a14e66f7d
abdelhadi-12 <147516788+abdelhadi-12@users.noreply.github.com>
2025-10-19 22:48:22 +0100

    Update API endpoint to production URL

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,7 +1,7 @@
 // src/lib/api.js
 import axios from "axios";
 
-export const API = "http://127.0.0.1:8000"; // 🧩 même adresse que celle de FastAPI
+export const API = "https://dashbord-back.onrender.com"; // 🧩 même adresse que celle de FastAPI
 
 export const http = axios.create({
   baseURL: API,
